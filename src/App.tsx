42ea9ebad071d4400007e4380be8081e730fe666
gye0n1 <134510838+gye0n1@users.noreply.github.com>
2025-03-15 17:13:19 +0900

    initial project setup

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import AppRouter from "./AppRouter";
+
+const App: React.FC = () => {
+  return (
+    <>
+      <h1>Welcome to My React App!</h1>
+      <AppRouter />
+    </>
+  );
+};
+
+export default App;
