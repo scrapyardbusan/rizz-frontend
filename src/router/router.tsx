5b155b2c1e181da642b33a6536dcd562fc53f79c
gye0n1 <134510838+gye0n1@users.noreply.github.com>
2025-03-16 10:04:58 +0900

    Build

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,15 +1,13 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "../pages/Home";
 import Result from "../pages/Result";
 
 const Router = () => {
   return (
-    <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/result" element={<Result />} />
-        </Routes>
-    </BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/result" element={<Result />} />
+    </Routes>
   );
 };
 
