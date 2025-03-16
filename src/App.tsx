e1ef0f4373ba315f0b28122e2930f30991f96496
gye0n1 <134510838+gye0n1@users.noreply.github.com>
2025-03-16 09:42:15 +0900

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Result from "./pages/Result";
+
 function App() {
   return (
-    <div>
-      test
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/result" element={<Result />} />
+      </Routes>
+    </Router>
   );
 }
 
